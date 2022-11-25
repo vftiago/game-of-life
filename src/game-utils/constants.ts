0e476abede7818adb5b7f2ef6f9068166e713881
tiago <vftiago@gmail.com>
2022-11-25 16:09:31 +0000

    🔧 slightly increase default interval

--- src/game-utils/constants.ts
+++ src/game-utils/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_COLUMN_COUNT = 50;
 export const DEFAULT_ROW_COUNT = 50;
-export const DEFAULT_INTERVAL = 150;
+export const DEFAULT_INTERVAL = 250;
 export const DEFAULT_DENSITY = 0.7;
 export const DEFAULT_CELL_SIZE = 14;

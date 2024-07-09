724e6d6dd4c8f98859877372711577074555f141
vftiago <vf.tiago@gmail.com>
2024-07-09 20:41:31 +0100

    ⬆️ update dependencies

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,5 +1,4 @@
 import { DEFAULT_COLUMN_COUNT, DEFAULT_ROW_COUNT } from "./constants";
-import "@testing-library/jest-dom";
 import { act, fireEvent, render, screen } from "./test-utils";
 import Game from "./Game";
 
